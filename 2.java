22ca7e897a7a7de4ec3c27e18561914d021ebec4
ShifaTaheniyath <taheniyathshifa785@gmail.com>
2025-06-23 19:09:53 +0530

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+     int n = 10;
+     System.out.println("print all odd numbers");
+     for(int i= 1; i<=n; i++){
+         if(i%2==1){
+             System.out.println(i);
+      }
+       }
+    }
+}
