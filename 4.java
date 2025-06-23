b5b378a04b5072c9ef5984638696d6c162a7af58
ShifaTaheniyath <taheniyathshifa785@gmail.com>
2025-06-23 19:11:01 +0530

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 8;
+        if(n%2==0){
+            System.out.println("n is odd");
+        }
+        else{
+            System.out.println("n is even");
+        }
+    }
+}
